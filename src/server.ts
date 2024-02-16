246aa311e9af8113e6d57a27a968ed2a8cd70371
AndriAlpian06 <61491000+AndriAlpian06@users.noreply.github.com>
2024-02-16 09:59:40 +0700

    third

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import TelegramBot from 'node-telegram-bot-api'
 import bodyParser from 'body-parser';
+import { error } from 'console';
 
 const app = express()
 const PORT = 3000;
@@ -8,9 +9,39 @@ const PORT = 3000;
 // setup bot telegram
 const botToken = '6364340921:AAHPBgdek2E5_qO_Bl_cmRnO0mlVyOX6Eak'
 const chatIds = ['5439991685', '5243622036', '6949185570']
-const bot = new TelegramBot(botToken, {polling: true})
+const bot = new TelegramBot(botToken, {polling: false})
 
 app.use(bodyParser.json())
+
+function startPolling(){
+    bot.getUpdates({ timeout: 10000, allowed_updates: []})
+    .then(handleUpdates)
+    .catch((error) => {
+        console.error('Error while polling:', error);
+        setTimeout(startPolling, 5000);
+    })
+}
+
+function handleUpdates(updates: any){
+    updates.forEach((update: any) => {
+        const message = update.message
+        const callbackQuery = update.callback_query
+
+        if(message){
+            const chatId = message.chat.id
+            const text = message.text
+
+            console.log(`Received message from ${chatId}: ${text}`)
+        } else if (callbackQuery){
+            const chatId = callbackQuery.chat.id
+            const data = callbackQuery.data
+
+            console.log(`Received callback query from ${chatId}: ${data}`)
+        }
+    })
+
+    startPolling()
+}
 
 app.post('/send-notification', (req, res) => {
     const message = req.body.message || 'Default Notification Message';
@@ -34,10 +65,9 @@ app.post('/send-notification', (req, res) => {
         console.error('Error sending notification to telegram', error)
         res.status(500).send('Error sending notification to telegram')
     }
-    
-    
 })
 
 app.listen(PORT, () => {
     console.log(`Server Running in PORT: ${PORT}`)
+    startPolling()
 })
